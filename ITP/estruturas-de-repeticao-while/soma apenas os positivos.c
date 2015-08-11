7d401b579e63d268dfb2da422dafc47e76cd0bf7
Elton Viana <eltonvs@outlook.com>
2015-08-11 19:55:44 -0300

    Create soma apenas os positivos.c

---
+++ ITP/estruturas-de-repeticao-while/soma apenas os positivos.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main (){
+    int n, temp, soma = 0, cont = 0;
+
+    scanf ("%i", &n);
+
+    while (cont < n){
+        scanf("%i", &temp);
+        if (temp > 0) {
+            soma += temp;
+        }
+        cont++;
+    }
+
+    printf ("%i", soma);
+
+    return 0;
+}
