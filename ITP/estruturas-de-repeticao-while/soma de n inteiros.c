59a58cf0434f4e420774a11b0023822fcca38d6c
Elton Viana <eltonvs@outlook.com>
2015-08-11 16:25:33 -0300

    Create soma de n inteiros.c

---
+++ ITP/estruturas-de-repeticao-while/soma de n inteiros.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main () {
+    int n, temp, soma = 0, cont = 0;
+
+    scanf ("%i", &n);
+
+    while (cont < n){
+        scanf("%i", &temp);
+        soma += temp;
+        cont++;
+    }
+
+    printf ("%i", soma);
+
+    return 0;
+}
