b5de98498b6e9143f9dae15d3892927d9d69a270
Elton Viana <eltonvs@outlook.com>
2015-08-11 20:03:19 -0300

    Create contando pares e impares de uma sequencia.c

---
+++ ITP/estruturas-de-repeticao-while/contando pares e impares de uma sequencia.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main () {
+    int n, temp, somapar = 0, somaimpar = 0, cont = 0;
+
+    scanf ("%i", &n);
+
+    while (cont < n) {
+        scanf("%i", &temp);
+        if (temp % 2 == 0)
+            somapar++;
+        else
+            somaimpar++;
+        cont++;
+    }
+
+    printf ("%i %i", somapar, somaimpar);
+
+    return 0;
+}
