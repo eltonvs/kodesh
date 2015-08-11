800b0ddfb51b83f449df1b2ded065cd9e3000566
Elton Viana <eltonvs@outlook.com>
2015-08-11 20:15:51 -0300

    Update MDC entre 2 numeros.c

--- ITP/estruturas-de-repeticao-while/MDC entre 2 numeros.c
+++ ITP/estruturas-de-repeticao-while/MDC entre 2 numeros.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
+
 int main() {
     int n, n1, n2, mdc, resto = 1;
 
     scanf("%i", &n1);
     scanf("%i", &n2);
     
-    while (resto != 0){
+    while (resto != 0) {
         resto = n1 % n2;
         if (resto == 0)
             mdc = n2;
