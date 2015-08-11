e3515ccd11dec2c68f0e6bbb6c58584f6e7d9b28
Elton Viana <eltonvs@outlook.com>
2015-08-11 16:02:54 -0300

    Create potencias de um inteiro.c

---
+++ ITP/estruturas-de-repeticao-while/potencias de um inteiro.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main (){
+    int x, n, multiplicador = 1, contador = 0;
+
+    scanf ("%i", &x);
+    scanf ("%i", &n);
+
+    while (contador < n){
+        multiplicador = x * multiplicador;
+        contador++;
+    }
+
+    printf ("%i", multiplicador);
+
+    return 0;
+}
