494f28651c5f9a4cb4b4cfdddce8b58a8f679512
Elton Viana <eltonvs@outlook.com>
2015-08-11 20:04:48 -0300

    Create loja de discos e dia de melhor faturamento.c

---
+++ ITP/estruturas-de-repeticao-while/loja de discos e dia de melhor faturamento.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main () {
+    int dia = 1, qtd, mdia = 0, mqtd = 0;
+
+    while (dia <= 30) {
+        scanf("%i", &qtd);
+        if (qtd > mqtd)
+            mdia = dia, mqtd = qtd;
+        dia++;
+    }
+
+    printf ("%i %i", mdia, mqtd);
+
+    return 0;
+}
