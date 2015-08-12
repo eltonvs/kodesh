39a24dd3a1f324b7da83c7d39587515da7d84e54
Elton Viana <eltonvs@outlook.com>
2015-08-12 20:49:10 -0300

    Create encontre o maximo para expressao.c

---
+++ ITP/estruturas-de-repeticao-for/encontre o maximo para expressao.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int x, y, m, n, e, m_e = 0, m_x, m_y;
+
+    scanf("%i", &m);
+    scanf("%i", &n);
+
+    for (x = 1; x <= m; x++)
+        for (y = 1; y <= n; y++) {
+            e = x*y-x*x+y;
+            if (e > m_e)
+                m_e = e, m_x = x, m_y = y;
+        }
+
+    printf("%i x=%i y=%i", m_e, m_x, m_y);
+
+    return 0;
+}
