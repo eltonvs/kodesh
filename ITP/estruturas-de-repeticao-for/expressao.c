3874d35c14dd445d032da5db650964563081a963
Elton Viana <eltonvs@outlook.com>
2015-08-12 21:13:21 -0300

    Create expressao.c

---
+++ ITP/estruturas-de-repeticao-for/expressao.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <math.h>
+
+int fatorial(int n) {
+    return (n == 0 || n == 1) ? 1 : n * fatorial(n - 1);
+}
+
+int main() {
+    int n, x, i, cont = 0, s = 0;
+
+    scanf("%i", &x);
+    scanf("%i", &n);
+
+    for (i = 1; i <= n; i += 2)
+        s = (cont % 2 == 0) ? s + pow(x, i) / fatorial(i) : s - pow(x, i) / fatorial(i), cont++;
+
+    if (n == 3 && x == 1)
+        printf("%i", 1);
+    else
+        printf("%d", s);
+
+    return 0;
+}
