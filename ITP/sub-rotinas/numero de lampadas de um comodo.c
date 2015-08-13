e9698c639e94a4943181ad2090716b7321086079
Elton Viana <eltonvs@outlook.com>
2015-08-13 08:35:32 -0300

    Create numero de lampadas de um comodo.c

---
+++ ITP/sub-rotinas/numero de lampadas de um comodo.c
@@ -0,0 +1,16 @@
+int num_lampadas(int classe, float larg, float prof) {
+    int pot, n = 1;
+    float i;
+
+    if (classe == 1)
+        pot = 15;
+    if (classe == 2)
+        pot = 18;
+    if (classe == 3)
+        pot = 20;
+    while (i <= pot) {
+        i = (n * 60) / (larg * prof);
+        n++;
+    }
+    return n - 1;
+}
