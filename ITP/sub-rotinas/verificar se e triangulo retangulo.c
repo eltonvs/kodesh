d17aec8c401aab6d4a1b0f1ea68e659fc3cec882
Elton Viana <eltonvs@outlook.com>
2015-08-13 19:51:40 -0300

    Create verificar se e triangulo retangulo.c

---
+++ ITP/sub-rotinas/verificar se e triangulo retangulo.c
@@ -0,0 +1,10 @@
+int ehTrianguloRetangulo (int a, int b, int c) {
+    if (a > b && a > c)
+        return (a*a == b*b + c*c) ? 1 : 0;
+    else if (b > a && b > c)
+        return (b*b == a*a + c*c) ? 1 : 0;
+    else if (c > a && c > b)
+        return (c*c == b*b + a*a) ? 1 : 0;
+    else
+        return 0;
+}
