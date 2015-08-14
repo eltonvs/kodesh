57320d0743506ee162de153b2753244693b0c53b
Elton Viana <eltonvs@outlook.com>
2015-08-14 19:02:05 -0300

    Create area de uma circunferencia.c

---
+++ ITP/sub-rotinas/area de uma circunferencia.c
@@ -0,0 +1,12 @@
+double calculaPI(int n) {
+    int i, impar = 1;
+    double pi = 0;
+    
+    for (i = 1; i <= n; i++)
+        pi = (i%2 == 0) ? pi - (double)4 / impar : pi + (double)4 / impar, impar += 2;
+    return pi;
+}
+
+double calculaArea(double pi, double raio) {
+    return pi * raio * raio;
+}
