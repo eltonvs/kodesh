be93eb535ffc25b02ce4c1f13a5783736a52acdd
Elton Viana <eltonvs@outlook.com>
2015-09-09 21:39:17 -0300

    Update maximo divisor comum entre varios inteiros.c
    Arrays was removed, 2 variables was used instead.

--- ITP/estruturas-de-repeticao-for/maximo divisor comum entre varios inteiros.c
+++ ITP/estruturas-de-repeticao-for/maximo divisor comum entre varios inteiros.c
@@ -1,26 +1,26 @@
 #include <stdio.h>
 
 int main() {
-    int n, n1, n2, mdc, resto, i, numeros[100];
+    int n, n1, n2, mdc, resto, i;
 
     scanf("%i", &n);
 
     for (i = 0; i < n; i++)
-        scanf("%i", &numeros[i]);
-
-    for (i = 0; i < (n - 1); i++) {
-        resto = 1, n1 = numeros[i], n2 = numeros[i + 1];
-        while (resto != 0) {
-            resto = n1 % n2;
-            if (resto == 0)
-                mdc = n2;
-            else
-                n1 = n2, n2 = resto;
+        if (i == 0) {
+            scanf("%i", &n1);
+        }else {
+            scanf("%i", &n2);
+            do {
+                resto = n1 % n2;
+                if (resto == 0)
+                    mdc = n2;
+                else
+                    n1 = n2, n2 = resto;
+            }while(resto != 0);
+            n1 = mdc;
         }
-        numeros[i + 1] = mdc;
-    }
 
-    printf("%i", numeros[n - 1]);
+    printf("%i", n1);
 
     return 0;
 }
