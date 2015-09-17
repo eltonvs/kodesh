c034452d34f9f7696a06cf1419c5ee8607c60403
Elton Viana <eltonvs@outlook.com>
2015-09-17 08:25:16 -0300

    Update sequencia crescente.c

--- ITP/estruturas-de-repeticao-for/sequencia crescente.c
+++ ITP/estruturas-de-repeticao-for/sequencia crescente.c
@@ -4,16 +4,14 @@ int main() {
     int n, n1, n2, i, seq;
     
     scanf("%i", &n);
+    scanf("%i", &n1);
     
-    for (i = 0; i < n; i++)
-        if (i == 0) {
-            scanf("%i", &n1);
-        }else {
-            scanf("%i", &n2);
-            seq = (n2 > n1) ? 1 : 0, n1 = n2;
-        }
-    
+    for (i = 0; i < n-1; i++) {
+        scanf("%i", &n2);
+        seq = (n2 > n1) ? 1 : 0, n1 = n2;
+    }
+
     printf("%i", seq);
-    
+
     return 0;
 }
