4c6a432885da697a98ba9c0a2d7335e5fc583f1c
Elton Viana <eltonvs@outlook.com>
2015-09-17 08:00:10 -0300

    Update maximo divisor comum entre varios inteiros.c

--- ITP/estruturas-de-repeticao-for/maximo divisor comum entre varios inteiros.c
+++ ITP/estruturas-de-repeticao-for/maximo divisor comum entre varios inteiros.c
@@ -4,16 +4,13 @@ int main() {
     int n, n1, n2, resto, i;
 
     scanf("%i", &n);
+    scanf("%i", &n1);
 
-    for (i = 0; i < n; i++)
-        if (i == 0) {
-            scanf("%i", &n1);
-        }else {
-            scanf("%i", &n2);
-            do
-                resto = n1 % n2, n1 = n2, n2 = resto;
-            while(resto != 0);
-        }
+    for (i = 0; i < n-1; i++)
+        scanf("%i", &n2);
+        do
+            resto = n1 % n2, n1 = n2, n2 = resto;
+        while(resto != 0);
 
     printf("%i", n1);
 
