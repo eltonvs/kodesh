47224919acb41f539bb481d3993e4f1bbd7fc749
Elton Viana <eltonvs@outlook.com>
2015-09-24 08:26:51 -0300

    Performance Improvements
    Added one boolean variable instead two auxiliar variables.

--- ITP/passagem-de-parametros/funcao divide.c
+++ ITP/passagem-de-parametros/funcao divide.c
@@ -1,6 +1,6 @@
 int divisao(int *m, int *n, int d) {
-    int t1 = *m, t2 = *n;
-    if((*m)%d == 0) *m = (*m)/d;
-    if((*n)%d == 0) *n = (*n)/d;
-    return (t1%d == 0 || t2%d == 0) ? 1 : 0;
+    int t = 0;
+    if((*m)%d == 0) *m = (*m)/d, t = 1;
+    if((*n)%d == 0) *n = (*n)/d, t = 1;
+    return t ? 1 : 0;
 }
