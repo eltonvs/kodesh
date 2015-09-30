c4073eded96a6eadc7d1c79b393ffd68969bd442
Elton Viana <eltonvs@outlook.com>
2015-09-30 19:15:20 -0300

    Create varios pontos de um polinomio.c

---
+++ ITP/arranjos/varios pontos de um polinomio.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    int n, k, i, j;
+    float r = 0;
+    scanf("%i", &n);
+    float p[n];
+    for (i = 0; i < n; i++) scanf("%f", &p[i]);
+
+    scanf("%i", &k);
+    float c[n];
+    for (i = 0; i < k; i++) scanf("%f", &c[i]);
+
+    for (i = 0; i < k; i++){
+        for (j = 0; j < n; j++)
+            r += p[j]*pow(c[i], j);
+        printf("%.2f ", r);
+        r = 0;
+    }
+
+    return 0;
+}
