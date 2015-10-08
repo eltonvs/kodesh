70579c4ab24d341c15e62d8cc2f535186f4f0047
Elton Viana <eltonvs@outlook.com>
2015-10-08 07:21:43 -0300

    Create validando um jogo de sudoku.c

---
+++ ITP/arranjos-multidimensionais-II/validando um jogo de sudoku.c
@@ -0,0 +1,54 @@
+int latino(int n, int m[n][n]) {
+    int i, j, k, vet[n];
+
+    for (i = 0; i < n; i++) {
+        vet[i] = 0;
+        for (j = 0; j < n; j++) {
+            vet[i] += m[i][j];
+            for (k = 0; k < n; k++)
+                if (m[i][j] == m[i][k] && j != k) return 0;
+        }
+    }
+    
+    for (i = 0; i < n - 1; i++)
+        if (vet[i] != vet[i + 1]) return 0;
+
+    for (j = 0; j < n; j++) {
+        vet[j] = 0;
+        for (i = 0; i < n; i++) {
+            vet[j] += m[i][j];
+            for (k = 0; k < n; k++)
+                if (m[i][j] == m[k][j] && i != k) return 0;
+        }
+    }
+    
+    for (i = 0; i < n - 1; i++)
+        if (vet[i] != vet[i + 1]) return 0;
+
+    return 1;
+}
+
+
+int soma_matriz(int n, int m[n][n]) {
+    int i, j, s = 0;
+
+    for (i = 0; i < n; i++)
+        for (j = 0; j < n; j++)
+            s += m[i][j];
+    return s;
+}
+
+int eh_sudoku(int sud[9][9]) {
+    int i, j, x, y, mini[3][3];
+
+    if (!latino(9, sud)) return 0;
+
+    for (x = 0; x <= 6; x += 3)
+        for (y = 0; y <= 6; y += 3) {
+            for (i = 0; i < 3; i++)
+                for (j = 0; j < 3; j++)
+                    mini[i][j] = sud[i+x][j+y];
+            if (soma_matriz(3, mini) != 45) return 0;
+        }
+    return 1;
+}
