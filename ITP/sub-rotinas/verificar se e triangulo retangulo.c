632dc8d0f779b142de80c6b889da580a4b72d094
Elton Viana <eltonvs@outlook.com>
2015-10-16 17:49:28 -0300

    Only one ternary operator

--- ITP/sub-rotinas/verificar se e triangulo retangulo.c
+++ ITP/sub-rotinas/verificar se e triangulo retangulo.c
@@ -1,8 +1,3 @@
 int ehTrianguloRetangulo (int a, int b, int c) {
-    if (a > b && a > c)
-        return (a*a == b*b + c*c) ? 1 : 0;
-    else if (b > a && b > c)
-        return (b*b == a*a + c*c) ? 1 : 0;
-    else
-        return (c*c == b*b + a*a) ? 1 : 0;
+    return (a*a == b*b + c*c || b*b == a*a + c*c || c*c == b*b + a*a) ? 1 : 0;
 }
