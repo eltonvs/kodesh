f568fd570fa5c8294fb954a1ec0ee24643778b08
Elton Viana <eltonvs@outlook.com>
2015-10-28 19:05:51 -0300

    Code improvements

--- 12 - Cadeia de caracteres/comparando string.c
+++ 12 - Cadeia de caracteres/comparando string.c
@@ -1,11 +1,11 @@
 int my_strcmp(char *str1, char *str2) {
-    int i = 0, l;
+    int i = 0, l = 0;
     while (str1[i] != 0 || str2[i] != 0)
         if (str1[i] > str2[i])
             return 1;
         else if (str1[i] < str2[i])
             return -1;
         else
-            l = 0, i++;
+            i++;
     return l;
 }
