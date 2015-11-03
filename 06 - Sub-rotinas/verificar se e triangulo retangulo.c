8457e37c0d6de5484be4e1132c0b166c33b210d3
Elton Viana <eltonvs@outlook.com>
2015-11-03 18:39:10 -0300

    Shorten Code

--- 06 - Sub-rotinas/verificar se e triangulo retangulo.c
+++ 06 - Sub-rotinas/verificar se e triangulo retangulo.c
@@ -1,3 +1,3 @@
 int ehTrianguloRetangulo (int a, int b, int c) {
-    return (a*a == b*b + c*c || b*b == a*a + c*c || c*c == b*b + a*a) ? 1 : 0;
+    return (a*a == b*b + c*c || b*b == a*a + c*c || c*c == b*b + a*a);
 }
