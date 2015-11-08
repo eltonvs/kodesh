038f87702efa8d9b823b40b97b6bc981a8b394ae
Elton Viana <eltonvs@outlook.com>
2015-11-08 16:38:19 -0300

    Shorten Code

--- 06 - Sub-rotinas/intervalo.c
+++ 06 - Sub-rotinas/intervalo.c
@@ -1,3 +1,3 @@
 int inter(int x, int y, int z) {
-    return ((x >= y && z >= y && z <= x) || (y > x && z >= x && z <= y)) ? 1 : 0;
+    return (x >= y && z >= y && z <= x) || (y > x && z >= x && z <= y);
 }
