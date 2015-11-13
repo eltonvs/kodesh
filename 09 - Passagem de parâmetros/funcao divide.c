09d2b9c1f4a4371f7aa1dedf6bbec95943132a92
Elton Viana <eltonvs@outlook.com>
2015-11-13 14:02:22 -0300

    Shorten Code

--- 09 - Passagem de parâmetros/funcao divide.c
+++ 09 - Passagem de parâmetros/funcao divide.c
@@ -1,6 +1,6 @@
 int divisao(int *m, int *n, int d) {
     int t = 0;
-    if((*m)%d == 0) *m = (*m)/d, t = 1;
-    if((*n)%d == 0) *n = (*n)/d, t = 1;
+    if(!(*m)%d) *m = (*m)/d, t = 1;
+    if(!(*n)%d) *n = (*n)/d, t = 1;
     return t ? 1 : 0;
 }
