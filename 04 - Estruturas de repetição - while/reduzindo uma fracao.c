1e0e25217f75ff1cc9db8a1770caf3f5d8bb127e
Elton Viana <eltonvs@outlook.com>
2015-11-15 09:58:54 -0300

    Shorten Code

--- 04 - Estruturas de repetição - while/reduzindo uma fracao.c
+++ 04 - Estruturas de repetição - while/reduzindo uma fracao.c
@@ -4,14 +4,14 @@
 int main () {
     int i, a, b;
 
-	scanf("%d", &a);
-	scanf("%d", &b);
-	
-	for (i = b; i >=1; i--)
-		if(a%i == 0 && b%i == 0)
-			a /= i, b /= i;
-	
-	printf("%d %d", a, b);
+    scanf("%d", &a);
+    scanf("%d", &b);
 
-	return 0;
+    for (i = b; i >=1; i--)
+        if(!a%i && !b%i)
+            a /= i, b /= i;
+
+    printf("%d %d", a, b);
+
+    return 0;
 }
