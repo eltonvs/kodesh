57ee2d095ae362e51b0d88f22caa3f05a4624995
Elton Viana <eltonvs@outlook.com>
2015-11-16 18:22:36 -0300

    Shorten Code

--- 13 - Arranjos multidimensionais I/detectando linhas e colunas nulas.c
+++ 13 - Arranjos multidimensionais I/detectando linhas e colunas nulas.c
@@ -14,10 +14,10 @@ int main() {
     for (i = 0; i < m; i++) {
         st1 = 0, st2 = 0;
         for (j = 0; j < n; j++)
-            st1 = (matriz[i][j] == 0) ? st1 : 1,
-            st2 = (matriz[j][i] == 0) ? st2 : 1;
-        ln = (st1 == 0) ? ln + 1 : ln,
-        cn = (st2 == 0) ? cn + 1 : cn;
+            st1 = !matriz[i][j] ? st1 : 1,
+            st2 = !matriz[j][i] ? st2 : 1;
+        ln = !st1 ? ln + 1 : ln,
+        cn = !st2 ? cn + 1 : cn;
     }
 
     printf("%i %i", ln, cn);
