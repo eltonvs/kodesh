bee455bd0e1e968f9b1a0c9f3dbe913d86829f80
Adelino Afonso <aafavelino@ufrn.edu.br>
2016-10-19 18:26:47 -0300

    Update area de uma circunferencia.c

--- 06 - Sub-rotinas/area de uma circunferencia.c
+++ 06 - Sub-rotinas/area de uma circunferencia.c
@@ -2,8 +2,10 @@ double calculaPI(int n) {
     int i, impar = 1;
     double pi = 0;
     
-    for (i = 1; i <= n; i++)
+    for (i = 1; i <= n; i++) {
         pi = (i%2 == 0) ? pi - (double)4 / impar : pi + (double)4 / impar, impar += 2;
+    }
+    
     return pi;
 }
 
